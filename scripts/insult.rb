63fe442d26d949cee4a6381262a3b55a2ecc1b94
Richard Lister <ric@burton-5.home>
2014-11-26 00:19:27 -0500

    be insulting

---
+++ scripts/insult.rb
@@ -0,0 +1,12 @@
+def insult(name)
+  insult = Nokogiri.HTML(open('http://www.pangloss.com/seidel/Shaker/')).xpath("//font").first.content.strip.gsub('\n', ', ')
+  "#{name}: #{insult}"
+rescue
+  [ "make love, not war", "no way, man", "chillax, dude", "relax", "peace out", "I'm a lover not a fighter" ].sample
+end
+
+desc 'botsy insult NAME', 'insult NAME in an erudite manner'
+hear /\bbotsy[,;:\s]+insult\s+(.+)/ do |mdata|
+  name = mdata[1].strip.split.map(&:capitalize).join(' ')
+  @caller.insult(name)
+end
