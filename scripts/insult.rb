77b46deb870502bbe5312e64c6da528803ccd340
Richard Lister <ric@burton-5.home>
2014-11-26 01:06:19 -0500

    remove debugging

--- scripts/insult.rb
+++ scripts/insult.rb
@@ -4,9 +4,8 @@ require 'nokogiri'
 def insult(name)
   insult = Nokogiri.HTML(open('http://www.pangloss.com/seidel/Shaker/')).xpath("//font").first.content.strip.gsub('\n', ', ')
   "#{name}: #{insult}"
-rescue => e
-  # [ "make love, not war", "no way, man", "chillax, dude", "relax", "peace out", "I'm a lover not a fighter" ].sample
-  e.message
+rescue
+  [ "make love, not war", "no way, man", "chillax, dude", "relax", "peace out", "I'm a lover not a fighter" ].sample
 end
 
 desc 'botsy insult NAME', 'insult NAME in an erudite manner'
